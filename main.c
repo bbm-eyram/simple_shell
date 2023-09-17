147956857670f6e93123dfb6614a7e576aa42e42
Abigail-Nyarkoh <nyarkohabby@email>
2023-09-17 13:18:02 +0100

    simple_shell

---
+++ main.c
@@ -0,0 +1,148 @@
+/*
+ * File: main.c
+ * Coding Team: 
+ *    - Ephraim Eyram (Lead Developer)
+ *    - Abigail Nyakoh (Development Team Member)
+ */
+
+#include "shell.h"
+
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
+/**
+ * sig_handler - Handles a signal to show a new prompt.
+ * @sig: The signal triggering the handler.
+ */
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/**
+ * execute - Executes a command in a child process.
+ *
+ * This function takes an array of args and the address of the
+ * starting of the args array. It forks a child process to execute
+ * the command, and returns the exit value of the executed command.
+ *
+ * @args: An array of args representing the command and its parameters.
+ * @front: A double pointer to the startinging of the args array.
+ *
+ * Return:
+ *   - If an error occurs, it returns a corresponding error code.
+ *   - Otherwise, it returns the exit value of the last executed command.
+ */
+int execute(char **args, char **front)
+{
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
+}
+
+/**
+ * main - This program simulates a simple UNIX command interpreter.
+ *
+ * @argc: The num of args supplied to the program.
+ * @argv: An array of pointers to the args.
+ *
+ * Return: The return value of the last executed command.
+ */
+int main(int argc, char *argv[])
+{
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
+
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
+
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
+}
---
+++ str_func1.c
@@ -0,0 +1,106 @@
+/*
+ * File: str_func1.c
+ * Coding Team: 
+ *    - Ephraim Eyram (Lead Developer)
+ *    - Abigail Nyakoh (Development Team Member)
+ */
+
+#include "shell.h"
+
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
+/**
+ * _strlen - Compute the length of a char str.
+ * @s: A pointer to the input str.
+ *
+ * This function calculates and returns the length of the char str
+ * pointed to by 's'. If 's' is NULL, it returns 0.
+ *
+ * Return: The length of the char str, or 0 if 's' is Null.
+ */
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
+}
+
+/**
+ * _strcpy - Custom str copying function that duplicates the source str
+ *           including the null terminator and stores it in the destination buffer.
+ *
+ * @dest: Pointer to the destination buffer where the string should be copied.
+ * @src: Pointer to the source str to be duplicated.
+ *
+ * Return: Pointer to the destination buffer (i.e., 'dest').
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concatenates two strs efficiently.
+ * @dest: A pointer to the destination str.
+ * @src: A pointer to the source str.
+ *
+ * This function appends the chars of the source str to the end of the
+ * destination str. It does so efficiently by iterating through both strs
+ * and making sure that the resulting destination str is Null-terminated.
+ *
+ * Returns: A pointer to the specified destination string.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp =  src;
+
+	while (*destTemp != '\0')
+		destTemp++;
+
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
+}
+
+/**
+ * _strncat - Concatenate 'n' bytes from the source to destination str.
+ *
+ * @dest: A pointer to the destination str.
+ * @src: A pointer to the source str.
+ * @n: The num of bytes to append from source.
+ *
+ * Description:
+ * This function appends up to 'n' bytes from the 'src' str to the end of
+ * the 'dest' str, ensuring proper null-termination. It modifies the
+ * 'dest' string in place and returns a pointer to the modified destination.
+ *
+ * Return: A pointer to the modified destination string.
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
